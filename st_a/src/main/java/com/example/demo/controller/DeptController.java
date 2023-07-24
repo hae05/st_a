f88c89ac42c971057b73b97aa25ca8689c39bc3b
yampt1111 <yampt1111@gmail.com>
2023-07-24 16:30:52 +0900

    도서등록 완료 [신혜정]

--- st_a/src/main/java/com/example/demo/controller/DeptController.java
+++ st_a/src/main/java/com/example/demo/controller/DeptController.java
@@ -5,19 +5,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dao.DeptDAO_JPA;
 import com.example.demo.dao.DeptDAO_MB;
+import com.example.demo.entity.Dept;
 
 import lombok.Setter;
 
 @Controller
 @Setter
 public class DeptController {
+	
 	@Autowired
 	private DeptDAO_JPA dao_JPA;
 	@Autowired
 	private DeptDAO_MB dao_MB;
+	
+	@GetMapping("/dept/insert")
+	public void insert() {
+		
+	}
+	@PostMapping("/dept/insert")
+	public String insert(Dept d) {
+		dao_JPA.save(d);
+		return "redirect:/dept/list";
+	}
 	
 	@GetMapping("/dept/list")
 	public void list(Model model) {
